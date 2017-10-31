2f1d7254670ffc100b2f056f529f1575fe7ed077
Mohitsharma44 <mohitsharma44@gmail.com>
2017-10-31 18:25:16 -0400

    bootstrapping ir controller in cli

--- uocontroller/cli/bootstrap.py
+++ uocontroller/cli/bootstrap.py
@@ -4,6 +4,8 @@
 # in this file in the same way as UOControllerBaseController.
 
 from uocontroller.cli.controllers.base import UOControllerBaseController
+from uocontroller.cli.controllers.ir import UOControllerIrController
 
 def load(app):
     app.handler.register(UOControllerBaseController)
+    app.handler.register(UOControllerIrController)
